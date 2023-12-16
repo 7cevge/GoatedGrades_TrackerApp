7eb8843268d36b9414995dc1aef1071a8a35878e
Tony7hang <111335240+7cevge@users.noreply.github.com>
2023-12-15 22:33:29 -0600

    update sql

--- SQLFile.sql
+++ SQLFile.sql
@@ -2,49 +2,72 @@ create database gradeTrackerApp;
 use gradeTrackerApp;
 
 create table users (
-	userName char not null,					# '7hang'
-    userPassword char not null,				# 'Zxc1032002'
-    userId int not null,					# '1'
+	userName varchar(20) not null,					# '7hang'
+    userPassword varchar(20),						# 'Zxc1032002'
+    userId int not null,							# '1'
     primary key (userid)			
 );
 
 create table semester (
-	semesterLabel char not null,			# 'Spring-25'
-    semesterNote char,						# "My Gpa be dropping bro..."
-	semesterId int not null,				# '1'
+	semesterLabel varchar(20) not null,				# 'Spring-25'
+    semesterNote text,								# "My Gpa be dropping bro..."
+	semesterId int not null,						# '1'
     userId int not null,
     primary key (semesterId),
     foreign key (userId) references users (userId)
 );
 
 create table class (
-	className char not null,				# 'Introduction to Algorithms'
-    classCode char not null,				# 'CS430'
-    classWeight int not null,				# '3'
-    classActualGrade char,					# 'B'
-    classNote char,							# "This class is taught by professor Bauer"
-	classId int not null,					# '1'
+	className varchar(50) not null,					# 'Introduction to Algorithms'
+    classCode varchar(10) not null,					# 'CS430'
+    classWeight int not null,						# '3'
+    classActualGrade char,							# 'B'
+    classNote text,									# "This class is taught by professor Bauer"
+	classId int not null,							# '1'
     semesterId int not null,
     primary key (classId),
     foreign key (semesterId) references semester (semesterId)
 );
 
 create table gradePartition (
-	partitionName char not null,			# 'Exams'
-    partitionPercent numeric not null,			# '60.5%'
-    partitionNote char,						# "There is three exams, each 20%!"
-	partitionId int not null,				# '1'
+	partitionName varchar(20) not null,				# 'Exams'
+    partitionPercent numeric(9,5) not null,			# '60.5%'
+    partitionNote text,								# "There is three exams, each 20%!"
+	partitionId int not null,						# '1'
     classId int not null,
     primary key (partitionId),
     foreign key (classId) references class (classId)
 );
 
 create table grade (
-	gradeGot numeric,						# '45.5'
-	gradeOutOf numeric not null,			# '50'
-	gradeNote char,							# "I screwed up on this assignment..."
-	gradeId int not null,
+	gradeGot numeric(9,5),							# '45.5'
+	gradeOutOf numeric(9,5) not null,				# '50'
+	gradeNote text,									# "I screwed up on this assignment..."
+	gradeId int not null,							# '1'
     partitionId int not null,
     primary key (gradeId),
     foreign key (partitionId) references gradePartition (partitionId)
-)
+);
+
+# testing - - - - - - - - - - - - - - - - - - - - -
+
+insert into users
+values ('7hang','','0');
+
+insert into semester
+values ('S24','','1','0');
+
+insert into class
+values ('name','label','3','','','1','1');
+
+insert into gradePartition
+values ('name','25','','1','1');
+
+insert into grade
+values ('6.75','10','fk','1','1');
+
+select * from users;
+select * from semester;
+select * from class;
+select * from gradePartition;
+select * from grade;
