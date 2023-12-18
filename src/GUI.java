56dc883606cfd33427433a20e395d775d80c7dc5
Tony7hang <111335240+7cevge@users.noreply.github.com>
2023-12-17 22:44:39 -0600

    working on gui
    exploring methods and stuff

--- src/GUI.java
+++ src/GUI.java
@@ -4,15 +4,28 @@ import javax.swing.*;
 public class GUI {
 	public GUI() {
 		JFrame frame = new JFrame();
-		
 		JPanel panel = new JPanel();
+		
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		frame.getRootPane().putClientProperty("JRootPane.titleBarBackground", new Color(23,180,252));
+		       frame.getRootPane().putClientProperty("JRootPane.titleBarForeground", Color.white);
+		
+		frame.setTitle("GradeTrackerApp");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(true);
+		frame.setSize(500,500);
+		frame.setVisible(true);
+		
+		ImageIcon appIcon = new ImageIcon("./graphics/temporaryIcon.jpg");
+		frame.setIconImage(appIcon.getImage());
+		
+		frame.getContentPane().setBackground(new Color(0x336699));
+		/*
 		panel.setBorder(BorderFactory.createEmptyBorder(150,150,50,150));
 		panel.setLayout(new GridLayout(0,1));
 		
 		frame.add(panel, BorderLayout.CENTER);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("test");
 		frame.pack();
-		frame.setVisible(true);
+		*/
 	}
 }
