4787834708bb6b6d6d248f862453c06621b9f6ee
Tony7hang <111335240+7cevge@users.noreply.github.com>
2023-12-18 15:04:39 -0600

    learning gui
    following bro code gui tutorial

--- src/GUI.java
+++ src/GUI.java
@@ -1,31 +1,52 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.*;
 
 public class GUI {
 	public GUI() {
 		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
+
+		/*
+		 * if want custom title bar, need frame to be undecorated and make a panel to do
+		 * the same thing the title bar would
+		 */
 		
-		JFrame.setDefaultLookAndFeelDecorated(true);
-		frame.getRootPane().putClientProperty("JRootPane.titleBarBackground", new Color(23,180,252));
-		       frame.getRootPane().putClientProperty("JRootPane.titleBarForeground", Color.white);
-		
+		// frame.setUndecorated(true);
+
 		frame.setTitle("GradeTrackerApp");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(true);
-		frame.setSize(500,500);
-		frame.setVisible(true);
+		frame.setSize(500, 500);
+		frame.setLayout(null);
 		
 		ImageIcon appIcon = new ImageIcon("./graphics/temporaryIcon.jpg");
 		frame.setIconImage(appIcon.getImage());
-		
+
 		frame.getContentPane().setBackground(new Color(0x336699));
-		/*
-		panel.setBorder(BorderFactory.createEmptyBorder(150,150,50,150));
-		panel.setLayout(new GridLayout(0,1));
 		
-		frame.add(panel, BorderLayout.CENTER);
-		frame.pack();
-		*/
+		JLabel label1 = new JLabel();
+		ImageIcon label1Img = new ImageIcon("./graphics/temporaryIcon.jpg");
+		Border label1Border = BorderFactory.createLineBorder(new Color(0xff00ff));
+		
+		label1.setText("label1 text");
+		label1.setIcon(label1Img);
+		label1.setIconTextGap(15);
+		
+		label1.setHorizontalAlignment(JLabel.CENTER);
+		label1.setVerticalAlignment(JLabel.CENTER);
+		label1.setBackground(new Color(0x000077));
+		label1.setForeground(new Color(0x993300));
+		label1.setOpaque(true);
+		label1.setBorder(label1Border);
+		label1.setBounds(25,25,400,400);
+		
+		label1.setHorizontalTextPosition(JLabel.CENTER);
+		label1.setVerticalTextPosition(JLabel.BOTTOM);
+		label1.setFont(new Font("MV Boli", Font.BOLD, 50));
+		
+		
+		frame.add(label1);
+		//frame.pack();
+		frame.setVisible(true);
 	}
 }
