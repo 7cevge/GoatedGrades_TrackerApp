cce9f218fa101fa1ca10d941fbe8f3aae9a4842f
Tony7hang <111335240+7cevge@users.noreply.github.com>
2023-12-19 00:04:52 -0600

    more work on title bar

--- src/GUI.java
+++ src/GUI.java
@@ -6,41 +6,35 @@ public class GUI {
 	public GUI() {
 		JFrame frame = new JFrame();
 
-		/*
-		 * if want custom title bar, need frame to be undecorated and make a panel to do
-		 * the same thing the title bar would
-		 */
-
-		// frame.setUndecorated(true);
-
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setUndecorated(false); // for custom title bar
 		frame.setResizable(true);
 		frame.setSize(900, 700);
 		frame.setLayout(null);
-
-		frame.getContentPane().setBackground(new Color(0x336699));
+		frame.getContentPane().setBackground(new Color(0x333333));
 
 		JLabel iconTitle = new JLabel();
 		ImageIcon icon = new ImageIcon(new ImageIcon("./graphics/temporaryIcon.jpg").getImage()
-				.getScaledInstance(25, 25, Image.SCALE_DEFAULT));
+				.getScaledInstance(20, 20, Image.SCALE_DEFAULT));
 
 		iconTitle.setText("Grade Tracker App - Alpha");
 		iconTitle.setIcon(icon);
-		iconTitle.setIconTextGap(15);
+		iconTitle.setIconTextGap(8);
 
 		iconTitle.setHorizontalAlignment(JLabel.LEFT);
 		iconTitle.setVerticalAlignment(JLabel.CENTER);
-		iconTitle.setForeground(new Color(0xFFFFFF));
-		iconTitle.setOpaque(false);
+		
+		iconTitle.setOpaque(true); // transparent background
 
-		iconTitle.setHorizontalTextPosition(JLabel.RIGHT);
+		iconTitle.setHorizontalTextPosition(JLabel.RIGHT); // text alignment in respect to image
 		iconTitle.setVerticalTextPosition(JLabel.CENTER);
-		iconTitle.setFont(new Font("MV Boli", Font.BOLD, 15));
+		iconTitle.setFont(new Font("MV Boli", Font.BOLD, 12));
+		iconTitle.setForeground(new Color(0xFFFFFF)); // set text color
+		iconTitle.setBounds(5, 0, 250, 20);
 
 		JPanel titleBar = new JPanel();
 		titleBar.setBackground(new Color(0x000000));
-		titleBar.setBounds(0, 0, 1500, 30);
-		titleBar.setLayout(new FlowLayout());
+		titleBar.setBounds(0, 0, 900, 24);
+		titleBar.setLayout(new FlowLayout(FlowLayout.LEFT, 3, 2));
 
 		titleBar.add(iconTitle);
 		frame.add(titleBar);
