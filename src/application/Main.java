09803b17cce2268dbdb88b02892e86e542aefc99
Tony7hang <111335240+7cevge@users.noreply.github.com>
2023-12-20 20:36:42 -0600

    brocode stages

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,43 @@
 package application;
-	
+
+import java.io.FileInputStream;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	public void start(Stage stage) {
+		try {
+			Group root = new Group();
+			Scene scene = new Scene(root, Color.GOLD);
+
+			Image icon;
+
+			icon = new Image(new FileInputStream("./graphics/temporaryIcon.jpg"));
+			stage.getIcons().add(icon);
+			stage.setTitle("title");
+			
+			stage.setWidth(720);
+			stage.setHeight(480);
+			stage.setResizable(true);
+			
+			stage.setFullScreen(false);
+			stage.setFullScreenExitHint("Press Esc to exit full screen");
+			stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("esc"));
+
+			stage.setScene(scene);
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
