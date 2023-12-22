499a6bf7e2dd1760e0571928c29c90e1ab5a498e
Tony7hang <111335240+7cevge@users.noreply.github.com>
2023-12-21 23:18:37 -0600

    fixed title bar

--- src/application/Control.java
+++ src/application/Control.java
@@ -3,15 +3,24 @@ package application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Control {
 	@FXML
 	private Button minimizeB = new Button();
-	
+
 	@FXML
 	private Button maximizeB = new Button();
+
+	@FXML
+	private AnchorPane titleBar;
+	
+	private double windowOffsetX, windowOffsetY;
 
 	public void minimize(ActionEvent e) {
 		Stage stage = (Stage) minimizeB.getScene().getWindow();
@@ -36,5 +45,20 @@ public class Control {
 		Platform.exit();
 		System.out.println("window has been closed");
 		System.out.println(e);
+	}
+
+	public void moveWindow1(MouseEvent e) {
+		windowOffsetX = e.getSceneX();
+	    windowOffsetY = e.getSceneY();
+	    System.out.println("titleBar was pressed: " + windowOffsetX + ", " + windowOffsetY);
+	    System.out.println(e);
+	}
+
+	public void moveWindow2(MouseEvent e) {
+		Stage stage = (Stage) titleBar.getScene().getWindow();
+	    stage.setX(e.getScreenX() - windowOffsetX);
+	    stage.setY(e.getScreenY() - windowOffsetY);
+	    System.out.println("titleBar was dragged to: " + stage.getX() + ", " + stage.getY());
+	    System.out.println(e);
 	}
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,11 @@
 package application;
 
-import java.io.FileInputStream;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 
 public class Main extends Application {
 	public static void main(String[] args) {
@@ -23,27 +15,11 @@ public class Main extends Application {
 	public void start(Stage stage) {
 		try {
 			stage.initStyle(StageStyle.UNDECORATED);
-			
+
 			Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			/*
-			 * Text text = new Text(); text.setText("text"); text.setX(25); text.setY(75);
-			 * text.setFont(Font.font("MV Boli", 75)); text.setFill(Color.web("0x26619C"));
-			 * 
-			 * Image icon;
-			 * 
-			 * icon = new Image(new FileInputStream("./graphics/temporaryIcon.jpg"));
-			 * stage.getIcons().add(icon); stage.setTitle("title");
-			 * 
-			 * stage.setWidth(720); stage.setHeight(480); stage.setResizable(true);
-			 * 
-			 * stage.setFullScreen(false);
-			 * stage.setFullScreenExitHint("Press Esc to exit full screen");
-			 * stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("esc"));
-			 * 
-			 * root.getChildren().add(text);
-			 */
+
 			stage.setScene(scene);
 			stage.show();
 		} catch (Exception e) {
