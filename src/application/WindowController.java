5e77afe065264b3849cbbdca1bb7438a95242934
Tony7hang <111335240+7cevge@users.noreply.github.com>
2024-05-13 22:36:16 -0500

    fix some stuff in window controller

--- src/application/WindowController.java
+++ src/application/WindowController.java
@@ -7,48 +7,47 @@ import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class WindowController {
 	@FXML
-	private Button minimizeButton, maximizeButton;
+	private Button maxBtn;
 
+	/* 
+		T = top, R = right, B = bottom, L = left, S = small, rh = resize handle
+		Named this way to make later functions with switch case easier to use 
+	*/
+	
 	@FXML
-	private AnchorPane titleBar;
-
-	/* T = top, R = right, B = bottom, L = left, S = small, rs = resize */
-	/* Named this way to make later functions with switch case easier to use */
-	@FXML
-	private Rectangle Trs, Rrs, Brs, Lrs;
+	private Rectangle rhT1, rhR2, rhB1, rhL2;
 
 	private double newX = -1, newY = -1;
 
-	/* The 3 window buttons */
+	/* ------------------------------------------------------------------- The 3 window buttons */
 	public void minimize(MouseEvent e) {
-		Stage stage = (Stage) minimizeButton.getScene().getWindow();
+		Stage stage = (Stage) maxBtn.getScene().getWindow();
 		stage.setIconified(true);
 	}
 
 	public void maximize(MouseEvent e) {
-		Stage stage = (Stage) maximizeButton.getScene().getWindow();
-		if (stage.isMaximized() && !maximizeButton.getStyleClass().contains("maximizeGraphic")) {
+		Stage stage = (Stage) maxBtn.getScene().getWindow();
+		if (stage.isMaximized() && !maxBtn.getStyleClass().contains("maxImg")) {
 			stage.setMaximized(false);
-			maximizeButton.getStyleClass().remove("restoreGraphic");
-			maximizeButton.getStyleClass().add("maximizeGraphic");
+			maxBtn.getStyleClass().remove("restoreImg");
+			maxBtn.getStyleClass().add("maxImg");
 		} else {
 			stage.setMaximized(true);
-			maximizeButton.getStyleClass().remove("maximizeGraphic");
-			maximizeButton.getStyleClass().add("restoreGraphic");
+			maxBtn.getStyleClass().remove("maxImg");
+			maxBtn.getStyleClass().add("restoreImg");
 		}
 	}
 
-	public void close(MouseEvent e) {
+	public void exit(MouseEvent e) {
 		Platform.exit();
 	}
 
-	/* Moving the window around the screen */
+	/* ---------------------------------------------------- Moving the window around the screen */
 	public void moveWindow1(MouseEvent e) {
 		String target = e.getTarget().toString();
 		if (target.contains("Rectangle")) {
@@ -60,7 +59,7 @@ public class WindowController {
 
 	public void moveWindow2(MouseEvent e) {
 		String target = e.getTarget().toString();
-		Stage stage = (Stage) titleBar.getScene().getWindow();
+		Stage stage = (Stage) maxBtn.getScene().getWindow();
 		if (target.contains("Rectangle")) {
 		} else if (newX != -1 && newY != -1) {
 			stage.setX(e.getScreenX() - newX);
@@ -70,55 +69,53 @@ public class WindowController {
 		}
 	}
 
-	/* Resizing the window */
+	/* -------------------------------------------------------------------- Resizing the window */
 	public void resizeWindow1(MouseEvent e) {
 		String target = e.getTarget().toString();
-		if (target.contains("id=TLrs") || target.contains("id=Trs") || target.contains("TRrs")
-				|| target.contains("id=SRrs") || target.contains("id=Rrs") || target.contains("id=BRrs")
-				|| target.contains("id=Brs") || target.contains("id=BLrs") || target.contains("id=Lrs")
-				|| target.contains("id=SLrs")) {
+		if (target.contains("id=rhT") || target.contains("id=rhR") || 
+			target.contains("id=rhB") || target.contains("id=rhL")) {
 			newX = e.getScreenX();
 			newY = e.getScreenY();
 		}
 	}
 
 	public void resizeWindow2(MouseEvent e) {
-		Stage stage = (Stage) titleBar.getScene().getWindow();
+		Stage stage = (Stage) maxBtn.getScene().getWindow();
 		if (newX != -1 && newY != -1) {
 			switch (e.getTarget().toString().substring(10, 17)) {
-			case "id=TLrs":
+			case "id=rhT1":
+				resize(stage, e, 'T');
+				break;
+			case "id=rhB1":
+				resize(stage, e, 'B');
+				break;
+			case "id=rhL1":
+				resize(stage, e, 'L');
+				break;
+			case "id=rhL2":
+				resize(stage, e, 'L');
+				break;
+			case "id=rhR1":
+				resize(stage, e, 'R');
+				break;
+			case "id=rhR2":
+				resize(stage, e, 'R');
+				break;
+			case "id=rhTL":
 				resize(stage, e, 'T');
 				resize(stage, e, 'L');
 				break;
-			case "id=Trs,":
-				resize(stage, e, 'T');
-				break;
-			case "id=TRrs":
+			case "id=rhTR":
 				resize(stage, e, 'T');
 				resize(stage, e, 'R');
 				break;
-			case "id=SRrs":
-				resize(stage, e, 'R');
-				break;
-			case "id=Rrs,":
-				resize(stage, e, 'R');
-				break;
-			case "id=BRrs":
-				resize(stage, e, 'B');
-				resize(stage, e, 'R');
-				break;
-			case "id=Brs,":
-				resize(stage, e, 'B');
-				break;
-			case "id=BLrs":
+			case "id=rhBL":
 				resize(stage, e, 'B');
 				resize(stage, e, 'L');
 				break;
-			case "id=Lrs,":
-				resize(stage, e, 'L');
-				break;
-			case "id=SLrs":
-				resize(stage, e, 'L');
+			case "id=rhBR":
+				resize(stage, e, 'B');
+				resize(stage, e, 'R');
 				break;
 			default:
 				System.err.println("Not resize bar");
@@ -135,24 +132,24 @@ public class WindowController {
 			} else {
 				stage.setHeight(stage.getHeight() + (stage.getY() - e.getScreenY()));
 				stage.setY(e.getScreenY());
-				Rrs.setHeight(stage.getHeight() + (stage.getY() - e.getScreenY()) - 28);
-				Lrs.setHeight(stage.getHeight() + (stage.getY() - e.getScreenY()) - 28);
+				rhR2.setHeight(stage.getHeight() + (stage.getY() - e.getScreenY()) - 28);
+				rhL2.setHeight(stage.getHeight() + (stage.getY() - e.getScreenY()) - 28);
 			}
 			break;
 		case 'R':
 			if (e.getScreenX() - stage.getX() < stage.getMinWidth()) {
 			} else {
 				stage.setWidth(e.getScreenX() - stage.getX());
-				Trs.setWidth(e.getScreenX() - stage.getX() - 8);
-				Brs.setWidth(e.getScreenX() - stage.getX() - 8);
+				rhT1.setWidth(e.getScreenX() - stage.getX() - 16);
+				rhB1.setWidth(e.getScreenX() - stage.getX() - 16);
 			}
 			break;
 		case 'B':
 			if (e.getScreenY() - stage.getY() < stage.getMinHeight()) {
 			} else {
 				stage.setHeight(e.getScreenY() - stage.getY());
-				Rrs.setHeight(e.getScreenY() - stage.getY() - 28);
-				Lrs.setHeight(e.getScreenY() - stage.getY() - 28);
+				rhR2.setHeight(e.getScreenY() - stage.getY() - 28);
+				rhL2.setHeight(e.getScreenY() - stage.getY() - 28);
 			}
 			break;
 		case 'L':
@@ -160,8 +157,8 @@ public class WindowController {
 			} else {
 				stage.setWidth(stage.getWidth() + (stage.getX() - e.getScreenX()));
 				stage.setX(e.getScreenX());
-				Trs.setWidth(stage.getWidth() + (stage.getX() - e.getScreenX()) - 8);
-				Brs.setWidth(stage.getWidth() + (stage.getX() - e.getScreenX()) - 8);
+				rhT1.setWidth(stage.getWidth() + (stage.getX() - e.getScreenX()) - 16);
+				rhB1.setWidth(stage.getWidth() + (stage.getX() - e.getScreenX()) - 16);
 			}
 			break;
 		default:
@@ -169,7 +166,7 @@ public class WindowController {
 		}
 	}
 
-	/* changing scenes */
+	/* ------------------------------------------------------------------------ Changing scenes */
 
 	@FXML
 	public ScrollPane contentScene;
